bf5fc6e33d6c770775e54055633b30185871ed0d
jank10 <jank10@gmx.net>
2024-05-06 18:44:34 +0200

    Update vom Code 1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! :-)');
+	res.send('Hello FS2024 DevOps Course! :-) Jan Küchli hat Anpassungen am Code gemacht.');
 });
 
 app.listen(PORT, HOST);
